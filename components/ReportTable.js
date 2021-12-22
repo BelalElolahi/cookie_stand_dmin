fd5b4d19661a0e6f5616f84cc29945cd7ea33501
BelalElolhi <belalelolahi003@gmail.com>
2021-12-22 14:57:36 +0200

    conect the bacj end with the fronta get data from the database and render it

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -1,6 +1,29 @@
+import axios from "axios"
+
+const baseUrl = 'https://cooke-stand.herokuapp.com/'
+const endPoint = baseUrl+'api/v1/cookie_stands/'
 export default function ReportTable(props) {
     let totalOfHours =[0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     let totalOfTotal = 0
+    
+    function config() {
+
+        return {
+          headers:{
+            'Authorization': `Bearer ${props.token.access}`
+          }
+        }
+    }
+    async function deleteResource(id){
+        try {
+            const url = endPoint + id;
+            await axios.delete(url, config()); 
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <>
 
@@ -25,7 +48,9 @@ export default function ReportTable(props) {
                         let total = 0
                         return(
                         <tr className='border border-collapse border-black table-auto bg-green-400'> 
-                            <td className='border border-collapse border-black table-auto'>{Branche.location}</td>
+                            <td className='border border-collapse border-black table-auto'>{Branche.location}{<svg onClick={()=>deleteResource(Branche.id)} xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>}</td>
                             { Branche.hourly_sales.map((item,indx)=>{
                                 total += item
                                 totalOfHours[indx] += item 
